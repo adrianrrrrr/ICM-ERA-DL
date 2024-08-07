990a16b13de2eb037d5be907830cc4c03a4955ae
Adrian Ramos <adrian.ramos.gonzalez@estudiantat.upc.edu>
2024-08-07 10:25:30 +0200

    Forgot the .gitignore to clean branch

--- ICM-ERA-DL/main.py
+++ ICM-ERA-DL/main.py
@@ -5,63 +5,30 @@ from preprocessing import *
 # do so in order to align all of us with having clear the current problems to work on
 
 import time
-import numpy as np
+#import numpy as np
 import matplotlib.pyplot as plt
 
 start = time.time()
 
-def generate_inputs_train(xgboost_params, input_var_names, train_input_dir, eval_input_dir, np_train_files_dir, np_eval_files_dir, prefix):
-    #xgb = XGBRegressor(**xgboost_params)
-    # TODO PSEUDOCODE: initialize the uNET with the default constructor
-
-    # Full model
-    xbg_train_generator = XGBoostInputGenerator(input_var_names, target_var_names, input_dir=train_input_dir,
-                                                output_dir=np_train_files_dir, downsample_ratio=0.1,
-                                                records_per_file=1e10,
-                                                seed=123, out_file_prefix=prefix)
-    train_np_flist = xbg_train_generator.generate_np_files()
-    xbg_eval_generator = XGBoostInputGenerator(input_var_names, target_var_names, input_dir=eval_input_dir,
-                                               output_dir=np_eval_files_dir, downsample_ratio=0.1,
-                                               records_per_file=1e10,
-                                               seed=123, out_file_prefix=prefix)
-    eval_np_flist = xbg_eval_generator.generate_np_files()
-    train_data = np.load(train_np_flist[0])
-    eval_data = np.load(eval_np_flist[0])
-
-def generate_inputs_train2(uNET_params, input_var_names, train_input_dir, eval_input_dir, np_train_files_dir, np_eval_files_dir, prefix):
-    # TODO PSEUDOCODE: initialize the uNET with the default constructor
-
+def generate_inputs_train(uNET_params, input_var_names, train_input_dir, eval_input_dir, np_train_files_dir, np_eval_files_dir, prefix):
     # Full model
     uNET_train_generator = uNETInputGenerator(input_var_names, target_var_names, input_dir=train_input_dir,
-                                                output_dir=np_train_files_dir, downsample_ratio=0.1,
+                                                output_dir=np_train_files_dir, downsample_ratio=1,
                                                 records_per_file=1e10,
                                                 seed=123, out_file_prefix=prefix)
     train_np_flist = uNET_train_generator.generate_np_files()
     uNET_eval_generator = uNETInputGenerator(input_var_names, target_var_names, input_dir=eval_input_dir,
-                                                output_dir=np_eval_files_dir, downsample_ratio=0.1,
+                                                output_dir=np_eval_files_dir, downsample_ratio=1,
                                                 records_per_file=1e10,
                                                 seed=123, out_file_prefix=prefix)
     eval_np_flist = uNET_eval_generator.generate_np_files()
     train_data = np.load(train_np_flist[0])
     eval_data = np.load(eval_np_flist[0])
 
-
-# TODO PSEUDOCODE: From here it is used the XGBRegressor from xgboost! I write in pseudocode for the uNET
-
-    # Forward each image in train_data['inputs'] then compare with the ground truth stored in train_data['targets'],
-    # also indicate the training set for computing the metrics and so
-'''
-    xgb.fit(train_data['inputs'], train_data['targets'], eval_set=[(eval_data['inputs'], eval_data['targets'])],
-            verbose=True)
-    evals_metrics = xgb.evals_result()
-    model_path = f"{save_model_folder}{prefix}1000.json"
-    xgb.save_model(model_path)
-
-
-    return xgb, evals_metrics, model_path
-'''
+# Agreed on last meeting
 input_var_names = ['lon', 'lat', 'eastward_model_wind', 'northward_model_wind', 'model_speed', 'model_dir',
                    'msl', 'air_temperature', 'q', 'sst', 'uo', 'vo']
+
 print("Number of input variables / features = ",len(input_var_names))
 
 scat_model_var_names = {'scat': ['eastward_wind', 'northward_wind'],
@@ -69,19 +36,17 @@ scat_model_var_names = {'scat': ['eastward_wind', 'northward_wind'],
 target_var_names = ['u_diff', 'v_diff']
 print("Number of output targets = ",len(target_var_names))
 
-# FROM HERE WE START TO LOAD & ADAPT THE DATA WITH THE GIVEN HELPER FUNCTIONS. BUT JUST FOR EDA FROM NOW
-
 #File path env setup. Currently local execution for Adrian's iMac
 # Train period from 02/01/2020 - 06/03/2020 both included
-train_input_dir = "/Volumes/SSD Adrian/TFM/adrian_tfm/ASCAT_l3_collocations/2020/train/"
+train_input_dir = "/Volumes/SSD iMac/TFM/adrian_tfm/ASCAT_l3_collocations/2020/train/"
 # Test period from  10/03/2020 - 01/05/2020 both included
-eval_input_dir = "/Volumes/SSD Adrian/TFM/adrian_tfm/ASCAT_l3_collocations/2020/test/"
+eval_input_dir = "/Volumes/SSD iMac/TFM/adrian_tfm/ASCAT_l3_collocations/2020/test/"
 
-np_train_files_dir = "/Volumes/SSD Adrian/TFM/adrian_tfm/ASCAT_l3_collocations/2020/uNET_np_data/train/"
-np_eval_files_dir = "/Volumes/SSD Adrian/TFM/adrian_tfm/ASCAT_l3_collocations/2020/uNET_np_data/test/"
+np_train_files_dir = "/Volumes/SSD iMac/TFM/adrian_tfm/ASCAT_l3_collocations/2020/uNET_np_data/train/"
+np_eval_files_dir = "/Volumes/SSD iMac/TFM/adrian_tfm/ASCAT_l3_collocations/2020/uNET_np_data/test/"
 
-plots_folder = "/Volumes/SSD Adrian/TFM/adrian_tfm/ASCAT_l3_collocations/2020/plots/uNET_importance/"
-save_model_folder = "/Volumes/SSD Adrian/TFM/adrian_tfm/ASCAT_l3_collocations/2020/saved_models/"
+plots_folder = "/Volumes/SSD iMac/TFM/adrian_tfm/ASCAT_l3_collocations/2020/plots/uNET_importance/"
+save_model_folder = "/Volumes/SSD iMac/TFM/adrian_tfm/ASCAT_l3_collocations/2020/saved_models/"
 file_prefix = "allvars_cpu_"
 
 # Let's define some hyper-parameters
@@ -95,11 +60,10 @@ hparams = {
 
 # Condition to generate the data only one time. A improvement may be to try to read folder and only generate
 # when folders do not exist
-if True:
-    generate_inputs_train2(hparams, input_var_names, train_input_dir, eval_input_dir, np_train_files_dir, 
+if False:
+    generate_inputs_train(hparams, input_var_names, train_input_dir, eval_input_dir, np_train_files_dir, 
                         np_eval_files_dir, prefix=file_prefix)
     
-#xgb = XGBRegressor(**xgboost_params) | Not yet
 train_fn = np_train_files_dir + "allvars_cpu_000.npz"
 val_fn = np_eval_files_dir + "allvars_cpu_000.npz"
 
@@ -107,3 +71,44 @@ train_data = np.load(train_fn)
 eval_data = np.load(val_fn)
 
 # Now I can play freely with the data. It is from here when I am free to write the any code I want
+
+# This is the unmasked train data (Input). In Shape = (995807, 12)
+train = train_data['inputs']
+# These are the input var names in a numpy.ndarray. Equivalent to the list input_var_names. Shape = (12,)
+ix = train_data['input_var_names']
+# This is the unmasked ground truth data (Input). Out Shape = (995807, 2)
+ground_truth = train_data['targets']
+
+for index in range(12):
+    mean = np.nanmean(train[:,index]) #Computes ignoring NaN values
+    std = np.nanstd(train[:,index]) #Computes ignoring NaN values
+    maxd = train[:,index].max()
+    mind = train[:,index].min()
+    print(ix[index]," variable mean =  ",mean, " ; std = ",std," ; max = ",maxd," min = ",mind)
+
+
+plt.hist(train[:,9], bins='auto')  # arguments are passed to np.histogram
+plt.title("Histogram of SST with 'auto' bins")
+plt.show()
+
+aux = ['u','v']
+for index in range(2):
+    mean = np.nanmean(ground_truth[:,index]) #Computes ignoring NaN values
+    std = np.nanstd(ground_truth[:,index]) #Computes ignoring NaN values
+    maxd = ground_truth[:,index].max()
+    mind = ground_truth[:,index].min()
+    print(aux[index]," ground truth mean =  ",mean, " ; std = ",std," ; max = ",maxd," min = ",mind)
+
+plt.hist(ground_truth[:,0], bins='auto')  # arguments are passed to np.histogram
+plt.title("Histogram of u component ground truth with 'auto' bins")
+plt.show()
+
+plt.hist(ground_truth[:,1], bins='auto')  # arguments are passed to np.histogram
+plt.title("Histogram of v component ground truth with 'auto' bins")
+plt.show()
+
+'''
+plt.imshow(train_data['inputs'])
+plt.colorbar()
+plt.show()
+'''
--- ICM-ERA-DL/preprocessing.py
+++ ICM-ERA-DL/preprocessing.py
@@ -3,9 +3,9 @@ import numpy as np
 from netCDF4 import Dataset
 from collections.abc import Iterable
 import os
-
-class XGBoostInputGenerator:
-    def __init__(self, input_var_names, target_var_names, input_grid='l2', input_dir=None,
+ 
+class uNETInputGenerator:
+    def __init__(self, input_var_names, target_var_names, input_grid='l3', input_dir=None,
                  output_dir=None, file_list=None, downsample_ratio=1, records_per_file=1e9, out_file_prefix="", seed=None, n_sigma=None):
         self.input_dir = input_dir
         self.output_dir = output_dir
@@ -13,7 +13,7 @@ class XGBoostInputGenerator:
                                     'model': ['eastward_model_wind', 'northward_model_wind']}
         self.input_var_names = input_var_names
         self.target_var_names = target_var_names
-        self.separator = '/' #Separator for unix
+        self.separator = '' # Noise. It was \\ (Incorrect). / is correct for unix, but is redundant
         self.downsample_ratio = downsample_ratio
         self.n_sigma = n_sigma
         self.seed = seed
@@ -28,7 +28,7 @@ class XGBoostInputGenerator:
             self.nc_reader = self.read_l3_nc_data
         else:
             self.nc_reader = self.read_l2_nc_data
-    def create_dir(self, dir):
+    def create_dir(self, dir): # sudo chmod a+rw /path/to/folder/that/you/want/to/write/to 
         if not os.path.exists(dir):
             os.makedirs(dir)
 
@@ -59,7 +59,9 @@ class XGBoostInputGenerator:
         f = Dataset(fpath)
         selected_data = []
         lon = f.variables['lon'].__array__()
+        #lon = lon[2568:2824] # Eugenia 31/07 mail
         lat = f.variables['lat'].__array__()
+        #lat = lat[864:1120] # Eugenia 31/07 mail
         lats_mesh, lons_mesh = np.meshgrid(lat, lon, indexing='ij')
         for idx, var in enumerate(var_names):
             print(idx, var)
@@ -77,7 +79,7 @@ class XGBoostInputGenerator:
         f.close()
         data = np.ma.stack(selected_data)
         return data
-
+    
     def calculate_target(self, fpath: str):
         scat_data = self.nc_reader(fpath, self.scat_model_var_names['scat'])
         model_data = self.nc_reader(fpath, self.scat_model_var_names['model'])
@@ -109,124 +111,6 @@ class XGBoostInputGenerator:
         input_data = input_data.transpose()
         targets = targets.transpose()
         return input_data, targets
-
-    def generate_np_files(self):
-        self.create_dir(self.output_dir)
-        accumulated_inputs = []
-        accumulated_targets = []
-        out_file_counter = 0
-        out_files = []
-        for fpath in self.file_list:
-            print(f"Processing {fpath}")
-            inputs, targets = self.generate_dataset(fpath)
-            # print(type(accumulated_inputs), accumulated_inputs)
-            accumulated_inputs.append(inputs)
-            accumulated_targets.append(targets)
-            if len(np.vstack(accumulated_targets)) >= self.records_per_file:
-                accumulated_inputs = np.vstack(accumulated_inputs)
-                accumulated_targets = np.vstack(accumulated_targets)
-                # Split array
-                chunk_ids = np.arange(0, len(accumulated_inputs), self.records_per_file).astype(int)
-                print(chunk_ids)
-                inputs_arr = np.split(accumulated_inputs, chunk_ids)
-                target_arr = np.split(accumulated_targets, chunk_ids)
-                print(len(inputs_arr))
-                # Save files
-                for idx in range(len(inputs_arr)):
-                    if len(inputs_arr[idx]) == self.records_per_file:
-                        np_data_path = f"{self.output_dir}{self.separator}{self.out_file_prefix}{out_file_counter:03d}.npz"
-                        print(f"saving rows inputs {inputs_arr[idx].shape} {target_arr[idx].shape} in {np_data_path}")
-                        np.savez(np_data_path, input_var_names=np.array(self.input_var_names), inputs=inputs_arr[idx],
-                                 targets=target_arr[idx])
-                        out_file_counter += 1
-                        out_files.append(np_data_path)
-                    else:
-                        accumulated_inputs = [inputs_arr[idx]]
-                        accumulated_targets = [target_arr[idx]]
-
-        accumulated_inputs = np.vstack(accumulated_inputs)
-        accumulated_targets = np.vstack(accumulated_targets)
-        np_data_path = f"{self.output_dir}{self.separator}{self.out_file_prefix}{out_file_counter:03d}.npz"
-        print(f"saving rows inputs {accumulated_inputs.shape} {accumulated_targets.shape} in {np_data_path}")
-        np.savez(np_data_path, input_var_names=np.array(self.input_var_names), inputs=accumulated_inputs,
-                 targets=accumulated_targets)
-        out_files.append(np_data_path)
-        return out_files
-    
-    
-class uNETInputGenerator:
-    def __init__(self, input_var_names, target_var_names, input_grid='l2', input_dir=None,
-                 output_dir=None, file_list=None, downsample_ratio=1, records_per_file=1e9, out_file_prefix="", seed=None, n_sigma=None):
-        self.input_dir = input_dir
-        self.output_dir = output_dir
-        self.scat_model_var_names = {'scat': ['eastward_wind', 'northward_wind'],
-                                    'model': ['eastward_model_wind', 'northward_model_wind']}
-        self.input_var_names = input_var_names
-        self.target_var_names = target_var_names
-        self.separator = '/' #Separator for unix
-        self.downsample_ratio = downsample_ratio
-        self.n_sigma = n_sigma
-        self.seed = seed
-        self.records_per_file = records_per_file
-        self.out_file_prefix = out_file_prefix
-        if input_dir:
-            self.file_list = glob(input_dir + '*.nc')
-            self.file_list.sort()
-        else:
-            self.file_list = file_list
-        if input_grid == "l3":
-            self.nc_reader = self.read_l3_nc_data
-        else:
-            self.nc_reader = self.read_l2_nc_data
-    def create_dir(self, dir):
-        if not os.path.exists(dir):
-            os.makedirs(dir)
-
-    def read_l2_nc_data(self, fpath: str, var_names: Iterable) -> np.array:
-        f = Dataset(fpath)
-        selected_data = []
-        hg_vars = ['se_model_wind_curl', 'se_model_wind_divergence']
-
-        for idx, var in enumerate(var_names):
-            var_data = f.variables[var].__array__()
-            var_data.data[var_data.mask] = np.nan
-            if var in hg_vars:
-                restored_var = np.full(f.variables['lon'].__array__().shape, np.nan)
-                var_data_cr = (var_data[:, 1:] + var_data[:, :-1]) / 2
-                var_data_cr.mask = var_data[:, 1:].mask & var_data[:, :-1].mask
-                var_data_cr = (var_data_cr[1:, :] + var_data_cr[:-1, :]) / 2
-                var_data_cr.mask = var_data_cr[1:, :].mask & var_data_cr[:-1, :].mask
-                var_data_cr[var_data_cr.mask] = np.nan
-                restored_var[1:-1, 1:-1] = var_data_cr
-                var_data = restored_var
-            #print(var, var_data.shape)
-            selected_data.append(var_data)
-        f.close()
-        data = np.ma.stack(selected_data)
-        return data
-
-    def read_l3_nc_data(self, fpath: str, var_names: Iterable) -> np.array:
-        f = Dataset(fpath)
-        selected_data = []
-        lon = f.variables['lon'].__array__()
-        lat = f.variables['lat'].__array__()
-        lats_mesh, lons_mesh = np.meshgrid(lat, lon, indexing='ij')
-        for idx, var in enumerate(var_names):
-            print(idx, var)
-            if var == 'lon':
-                #print("adding lon mesh")
-                selected_data.append(lons_mesh)
-            elif var == 'lat':
-                #print("adding lat mesh")
-                selected_data.append(lats_mesh)
-            else:
-                var_data = f.variables[var].__array__()
-                mask = var_data.mask
-                var_data.data[mask] = np.nan
-                selected_data.append(np.squeeze(var_data, axis=0))
-        f.close()
-        data = np.ma.stack(selected_data)
-        return data
     
     def generate_np_files(self):
         self.create_dir(self.output_dir)
