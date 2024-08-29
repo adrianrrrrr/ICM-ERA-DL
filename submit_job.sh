a02df8d4df2781371e06552b2f917ae165b2c9ae
Adrian Ramos <adrian.ramos.gonzalez@estudiantat.upc.edu>
2024-08-29 16:58:10 +0200

    Checking cuda availability on the cluster. envs installed

---
+++ submit_job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -p gpi.compute
+#SBATCH -J train_UNET
+#SBATCH --mem=32gb
+#SBATCH --gres=gpu:1
+#SBATCH -e "logs/%x-%j.e"
+#SBATCH -o "logs/%x-%j.o"
+module load conda/2022.10
+source .bashrc
+conda activate erasdl_unet_cuda
+python /home/usuaris/imatge/adrian.ramos/ICM-ERA-DL/EDA.py
